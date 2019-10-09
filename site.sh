1e6f394ee1f4947d749a49185d5627250600e573
Bharat Kunwar <brtknr@bath.edu>
2019-10-09 16:17:14 +0000

    Keep trying to delete cluster template until it returns 404

--- site.sh
+++ site.sh
@@ -1,13 +1,13 @@
 set -x
 
-kube_tag=${kube_tag:-v1.15.4}
+kube_tag=${kube_tag:-v1.16.0}
 image=`openstack image list --property os_distro=$os_distro -f value -c Name`
 nodes=${nodes:-2}
 for nd in flannel calico; do
     name=k8s-$nd-$os_distro
     ctname=$name-$kube_tag
     if [ "$action" == "create" ]; then
-        if [[ -z $(openstack coe cluster template list | grep $name) ]]; then
+        if [[ -z $(openstack coe cluster template list | grep $ctname) ]]; then
             openstack coe cluster template create --floating-ip-enabled --external-network public --fixed-network private --fixed-subnet private-subnet --image $image --flavor ds2G --master-flavor ds2G --docker-storage-driver overlay2 --coe kubernetes --network-driver $nd --labels kube_tag=$kube_tag $ctname
         fi
         if [[ -z $(openstack coe cluster list | grep COMPLETE | grep $name) ]]; then
@@ -15,6 +15,8 @@ for nd in flannel calico; do
         fi
     elif [ "$action" == "destroy" ]; then
         openstack coe cluster delete $name
-        openstack coe cluster template delete $ctname
+        while [ ! -z "$(openstack coe cluster template delete $ctname | grep 'HTTP 404')" ]; do sleep 10; done &
     fi
 done
+openstack coe cluster template list
+openstack coe cluster list
