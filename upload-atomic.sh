4de8900e80a41167cc0c469c8fe4d9e37be662ae
Bharat Kunwar <brtknr@bath.edu>
2019-12-03 14:14:59 +0000

    Change the way that images are uploaded

--- upload-atomic.sh
+++ upload-atomic.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-IMAGE=${IMAGE:-Fedora-Atomic-27-20180419.0}
+DATE=${DATE:-20191126.0}
+IMAGE=${IMAGE:-Fedora-AtomicHost-29-$DATE}
 ARCH=${ARCH:-x86_64}
-curl -OL https://download.fedoraproject.org/pub/alt/atomic/stable/$IMAGE/CloudImages/x86_64/images/$IMAGE.$ARCH.qcow2
+FNAME=$IMAGE.$ARCH.qcow2
+if [ ! -f $FNAME ]; then
+    curl -OL https://dl.fedoraproject.org/pub/alt/atomic/stable/Fedora-29-updates-$DATE/AtomicHost/$ARCH/images/$FNAME
+fi
 openstack image create \
-    --disk-format=qcow2 \
-    --container-format=bare \
-    --file=$IMAGE.$ARCH.qcow2\
-    --property os_distro='fedora-atomic' \
-    $IMAGE.$ARCH
+     --disk-format=qcow2 \
+     --container-format=bare \
+     --file=$FNAME \
+     --property os_distro=fedora-atomic --property hw_rng_model=virtio \
+     $IMAGE
